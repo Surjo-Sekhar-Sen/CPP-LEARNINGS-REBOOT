06bafd03bb3da26b073311d8d34713fb65289677
Surjo-Sekhar-Sen <surjosen400@gmail.com>
2026-07-01 14:20:27 +0530

    few updates in program and notes

--- 03_BASICS/16_ternary_operator.cpp
+++ 03_BASICS/16_ternary_operator.cpp
@@ -2,7 +2,10 @@
 using namespace std;
 
 int main(){
-    int age = 19;
-    (age > 18) ? cout << "Can Vote!!" : cout << "Cannot Vote!!";
+    // int age = 19;
+    // (age > 18) ? cout << "Can Vote!!" : cout << "Cannot Vote!!";
+
+    int x = 10, y = 20;
+    (x > y) ? cout << "x is greater than y" : cout << "y is greater than x";
     return 0;
 }
