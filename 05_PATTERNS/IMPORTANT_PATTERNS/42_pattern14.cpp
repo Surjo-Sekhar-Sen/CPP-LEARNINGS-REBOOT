02550e7c7e51601cb23940eaa89022bb4e316346
Surjo-Sekhar-Sen <surjosen400@gmail.com>
2026-07-04 12:42:45 +0530

    practiced hard patterns

--- 05_PATTERNS/IMPORTANT_PATTERNS/42_pattern14.cpp
+++ 05_PATTERNS/IMPORTANT_PATTERNS/42_pattern14.cpp
@@ -1,16 +1,14 @@
 #include <iostream>
 using namespace std;
-//FLOYD'S TRIANGLE
+//BINARY 0/1 BIT TRIANGLE
 
 int main(){
     int n;
-    int count = 1;
     cout << "Number of rows you want to print: ";
     cin >> n;
     for(int i = 1; i <= n; i++){
-        for(int j = 1; j <= i; j++){
-            cout << count << " ";
-            count++;
+        for(int j = i; j >= 1; j--){
+            cout << (j%2) << " ";
         }
         cout << "\n";
     }
