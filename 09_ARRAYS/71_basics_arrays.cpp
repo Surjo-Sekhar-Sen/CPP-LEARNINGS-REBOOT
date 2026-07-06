2322483717d64bcc8c4233ef7f5d130a4cc96691
Surjo-Sekhar-Sen <surjosen400@gmail.com>
2026-07-07 04:10:54 +0530

    started basics of arrays

---
+++ 09_ARRAYS/71_basics_arrays.cpp
@@ -0,0 +1,17 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr1[100]; //WE CAN DECLARE ANY ARRAY WITH SPECIFYING IT'S SIZE!!
+    arr1[0] = 10;
+    arr1[20] = 100; //IN ALL THE OTHER SPACES THERE WILL BE GARBAGE VALUE INSIDE!!
+    int arr2[5] = {1, 2, 3, 4, 5}; 
+    int arr3[] = {1, 2, 3, 4, 5, 6}; //IF WE DON'T SPECIFY SIZE THEN WE NEED
+    //TO GIVE THE VALUES IN IT!!
+
+    //ACCESSING
+    cout << arr1[0] << " " << arr1[30] << endl; 
+    cout << arr2[0] << " " << arr2[1] << " " << arr2[2] << " " << arr2[3] << " " << arr2[4] << endl;
+    cout << arr3[3];
+    return 0;
+}
---
+++ 09_ARRAYS/73_array_sum.cpp
@@ -0,0 +1,25 @@
+#include <iostream>
+using namespace std;
+
+int  main(){
+    int n, sum = 0;
+    cout << "Enter number of elements in the array: ";
+    cin >> n;
+
+    int* arr = new int[n];  //WE CANNOT DIRECTLY WRITE int arr1[n] BCOZ THIS IS CONSIDERED
+    //AS A BAD PRACTICE AND CAUSE COMPILER ERROR IN SOME CASES!!
+    cout << "Enter array elements: ";
+    for(int i = 0; i <= (n-1); i++){
+        cin >> arr[i];
+    }
+    cout << "Array elements entered: ";
+    for(int i = 0; i <= (n-1); i++){
+        cout << arr[i] << " ";
+    }
+
+    for(int i = 0; i <= (n-1); i++){
+        sum = sum + arr[i];
+    }
+    cout << "Sum of the array: " << sum;
+    return 0;
+}
