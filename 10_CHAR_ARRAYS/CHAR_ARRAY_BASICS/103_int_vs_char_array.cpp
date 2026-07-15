8aad4b6063a6d20bba16843106c736e6b39f9172
Surjo-Sekhar-Sen <surjosen400@gmail.com>
2026-07-15 20:33:27 +0530

    practice dynamic allocation in 1d char arrays

---
+++ 10_CHAR_ARRAYS/CHAR_ARRAY_BASICS/103_int_vs_char_array.cpp
@@ -0,0 +1,10 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int arr[4] = {1, 2, 3, 4};
+    char chrr[4] = "sen";
+    cout << arr << endl; //PRINTS THE BASE ADDRESS OF THE ARRAY!!
+    cout << chrr; //PRINTS THE CHARACTERS OF IT'S ARRAY!!
+    return 0;
+}
