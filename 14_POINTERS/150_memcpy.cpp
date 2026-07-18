6269cdf6fa11dab62851b02d0f409812964f8455
Surjo-Sekhar-Sen <surjosen400@gmail.com>
2026-07-18 18:54:30 +0530

    learnt about memcpy function and notes update

--- 14_POINTERS/150_memcpy.cpp
+++ 14_POINTERS/150_memcpy.cpp
@@ -7,9 +7,32 @@ int main(){
     int *iptr1 = &a;
     int b, c;
     int *iptr2 = &b;
-    memcpy(iptr1, iptr2, sizeof(a));
-    cout << b << " " << *iptr2 << endl;
-    memcpy(&a, &c, sizeof(a));
-    cout << c << " " << &c << *iptr2;
+    memcpy(iptr2, iptr1, sizeof(a)); //memcpy HAS (dest add, soource add, size in bytes)!!
+    cout << b << " " << *iptr2 << endl; //IMPORTANT IS THAT THE source AND dest BOTH NEEDS TO BE A POINTER ADDRESS!!
+    memcpy(&c, &a, sizeof(a));
+    cout << c << " " << &c << " " << *iptr2 << endl;
+
+    int arr[5] = {1, 2, 3, 4, 5};
+    int arrdest1[5];
+    int arrdest2[5];
+    int arrdest3[5];
+    memcpy(arrdest1, arr, 8); //IF WE SAY 8 BYTES THEN IT MAY COPY TILL 2 BOXES AND LEAVE OTHER AS IT IS!!
+    //ALSO THE SIZE HOW MUCH TO COPIED IS VERY ESSENTIAL AS IF THE USER PUTS WRONG OR OUT OF BOUND THEN 
+    //IT WOULD CREATE A BIG PROBLEM!!
+    for(int i = 0; i <= (5-1); i++){
+        cout << arrdest1[i] << " ";
+    }
+    cout << endl;
+
+    memcpy(arrdest2, arr, 16);
+    for(int i = 0; i <= (5-1); i++){
+        cout << arrdest2[i] << " ";
+    }
+    cout << endl;
+
+    memcpy(arrdest3, arr, 20);
+    for(int i = 0; i <= (5-1); i++){
+        cout << arrdest3[i] << " ";
+    }
     return 0;
 }
