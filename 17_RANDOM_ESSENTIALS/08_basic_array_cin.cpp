c9662018c3193ddb16c2a158a1708649241bde61
Surjo-Sekhar-Sen <surjosen400@gmail.com>
2026-08-14 00:36:09 +0530

    revising arrays

--- 17_RANDOM_ESSENTIALS/08_basic_array_cin.cpp
+++ 17_RANDOM_ESSENTIALS/08_basic_array_cin.cpp
@@ -7,5 +7,7 @@ int main(){
         cin >> arr[i];
         cout << arr[i] << " ";
     }
+    //INTERESTING WAY OF TAKING AND PRINTING VALUES!!
+    //IF YOU TAKE EVERYTHING FROM INPUT BUFFER IN ONE GO THEN THE OUTPUT WILL ALSO BE CONTINUOS!!
     return 0;
 }
