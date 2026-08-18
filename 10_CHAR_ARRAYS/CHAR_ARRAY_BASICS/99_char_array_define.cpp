7676326bfe71a11ba58d9c93910b9d8fa45d1341
Surjo-Sekhar-Sen <surjosen400@gmail.com>
2026-08-18 12:33:31 +0530

    revise char arrays

--- 10_CHAR_ARRAYS/CHAR_ARRAY_BASICS/99_char_array_define.cpp
+++ 10_CHAR_ARRAYS/CHAR_ARRAY_BASICS/99_char_array_define.cpp
@@ -2,13 +2,13 @@
 using namespace std;
 
 int main(){
-    char arr[6] = {'s', 'u', 'r', 'j', 'o', '\0'}; //\0 IS ESSENTIAL FOR PRINTING 
+    char arrbhosda[7] = {'N', 'e', 'e', 'l', 'e', 's', 'h'}; //\0 IS ESSENTIAL FOR PRINTING 
     //CHAR ARRAY WITHOUT ANY LOOP!!
     //IF YOU WANT TO PRINT IT WITH LOOP THEN \0 ISN'T NECESSARY!!
-    cout << arr[0] << " "; //GIVES FIRST ELEMENT!!
-    cout << arr[5] << " "; //GIVES NULL VALUE!! IF U CHANGE IT TO INTEGER THEN YOU WILL SEE 0!!
-    cout << int(arr[5]) << " ";
-    cout << arr << endl; //PRINTS FULL ARRAY WITHOUT LOOP!!
+    cout << arrbhosda[0] << " "; //GIVES FIRST ELEMENT!!
+    cout << arrbhosda[5] << " "; //GIVES NULL VALUE!! IF U CHANGE IT TO INTEGER THEN YOU WILL SEE 0!!
+    cout << int(arrbhosda[5]) << " ";
+    cout << arrbhosda << endl; //PRINTS FULL ARRAY WITHOUT LOOP!!
 
     //WITH LOOP
     char arr2[4] = {'g', 'o', 'a', 'l'};
