e95afe022698f113b8fd2e503c0eeb4186bf5a5e
Surjo-Sekhar-Sen <surjosen400@gmail.com>
2026-08-28 18:08:01 +0530

    updated the atoi function with error handling

--- 17_RANDOM_ESSENTIALS/11_char_arr_int.cpp
+++ 17_RANDOM_ESSENTIALS/11_char_arr_int.cpp
@@ -6,13 +6,21 @@ using namespace std;
 int arrtoint(char arr[], int size){
     int num = 0;
     int j = 0;
-    for(int i = (size-2); i >= 0; i--){
-        if(arr[i] == '-'){
-            num = -num;
-        }
-        else{
+    if(arr[0] != '-'){
+        for(int i = (size-3); i >= 0; i--){
             int digit = (arr[i] - int('0'));
             num = digit * round(pow(10, j++)) + num;
+        }
+    }
+    else{
+        for(int i = (size-2); i >= 0; i--){
+            if(arr[i] == '-'){
+                num = -num;
+            }
+            else{
+                int digit = (arr[i] - int('0'));
+                num = digit * round(pow(10, j++)) + num;
+            }
         }
     }
     return num;
@@ -55,22 +63,26 @@ int main(){
 
     if(cin >> n){
         cout << "Your input was: " << n << endl;
+        if(n <= 0){
+            cout << "Not a valid length!!"<< endl;
+            return 0;
+        }
     }
 
-    char* arr = new char[n+1];
+    char* arr1 = new char[n+2];
 
     cout << "Enter elements of the array as numbers: " << endl;
-    cin >> arr;
+    cin >> arr1;
     
-    if(checkvalid(arr, n+1) == true){
-        int numval = arrtoint(arr, n+1);
+    if(checkvalid(arr1, n+2) == true){
+        int numval = arrtoint(arr1, n+2);
         cout << "The number after self made atoi operations: ";
-        cout << numval;
+        cout << numval << endl;
     }
     else{
-        cout << "Ye Wrong number hai!!";
+        cout << "Ye Wrong number hai!!" << endl;
     }
 
-    delete[] arr;
+    delete[] arr1;
     return 0;
 }
